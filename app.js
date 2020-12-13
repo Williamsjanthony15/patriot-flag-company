5d5d82b8cc651ae93d6160c7b05ca694880cc20f
Anthony Joel Williams <williamsjanthony15@yahoo.com>
2020-12-12 20:01:34 -0600

    small JS add's, to get the site asking questions in console

--- app.js
+++ app.js
@@ -3,4 +3,16 @@
 
 
 var PatriotsFC;
+PatriotsFC = "Welcome to Patriotic Flag Company")
+console.log(1776)
 
+var promtquestion = "What year was the Army established?"
+var (1776) = prompt ("What year was the Army established?");
+console.log(1776);
+if (response === "1776") {
+    console.log("Congradulations")
+} else if (response === "1775") {
+    console.log("You're Wrong"); 
+} else { 
+    console.log("You're not even close")
+}
