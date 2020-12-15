d33771c1928047538bad8e16562b4b733e88502e
Anthony Joel Williams <williamsjanthony15@yahoo.com>
2020-12-15 02:40:49 -0600

    played around with functions. Have a lot of work to do. Not fully understanding them. Will continue to work

--- app.js
+++ app.js
@@ -1,22 +1,42 @@
 console.log("Hey im actually doing this!")
 
 // Here is the function and you declared it but you havent called it. Call it by >> sayHello(); Doing the function that you identified.
-function taskOne() {
-    var promptQuestion = "How American are you?";
-    var response = prompt(promptQuestion);
-} 
-var alertMessage;
-var color; 
 
-
-if (response === "As American As it Gets") {
-    alertMessage = "Well, You have an American Day";
-    color = "blue";
-}   else {
-    alertMessage = "That's Alright, America is land of the Free and home of the Brave"
-    color = "red";
+function add(a, b) {
+    var result = a + b;
+    return(result);
+    
 }
-document.getElementById("content").style.backgroundColo = color;
+    var result = ("17" + 76);
+   alert(result);
+
+
+// function taskOne(response) {
+
+//if (response === "As American As it Gets") {
+  //  alertMessage = "Well, You have an American Day";
+    //color = "blue";
+// }   //else {
+    //alertMessage = "That's Alright, America is land of the Free and home of the Brave";
+    //color = "red";
+//}
+
+//document.getElementById("result").style.backgroundColor = red;
+
+//function addPresidentsToDocument (President) {
+  //  document.write("<selection> \
+    //                        <p1>" + President + "</p1> \
+      //                      <img presidentImg" + President + ".png\*> \
+        //                    <h1>" + President + " Was the best!</h1> \
+          //                  </selection>"); 
+//}
+
+//addPresidentsToDocument("Roosevelt");
+//addPresidentsToDocument("Lincon");
+//addPresidentsToDocument("Washington");
+
+// = url("https://cdn.britannica.com/76/4476-050-E643DD49/Betsy-Ross-legend-flag.jpg")
+
 
 
 // Also done correctly but no call or declare for the colors. Your not saying what the colors are for nor what there supposed to do. Take the free code that JS gives you.... document.getElecmentById("content").style.backgroundColo = color;
