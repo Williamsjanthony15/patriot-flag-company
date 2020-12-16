acadc03a2e0a8c077d3a0a362c5a6a01393dfab9
Anthony Joel Williams <williamsjanthony15@yahoo.com>
2020-12-16 16:51:55 -0600

    reconfigured my code, i wanted to started again, with the same premis. I needed to clean it up

--- app.js
+++ app.js
@@ -1,62 +1,18 @@
-console.log("Hey im actually doing this!")
+var myPrompt = "Patriot Flag Company";
+console.log(myPrompt);
+var alertMessage; 
 
-// Here is the function and you declared it but you havent called it. Call it by >> sayHello(); Doing the function that you identified.
-
-// function add(a, b) {
-//     var result = a + b;
-//     return(result);
-    
-// }
-//     var result = ("17" + 76);
-//    alert(result);
-
-// for (var A = 0; A < 10; A+++) {
-//   document.write(A)
-// }
-for (var A = 0; A <= 100; A++) {
-  console.log(A);
+var promptQuestion = "What year was the American Constitution signed?";
+var response = prompt("what year was the American Constitution signed"); {
+  document.log() 
 }
 
 
-    do {
-      function validateUserPassword() {
-        var passwd = "13 Orginal Colonies";
-        var pass;
-    }while(validateUserPassword)
+if (response === "1787") {
+  alertMessage = "Correct! Welcome!";
+  backgroundColor = blue;
 
-
-// var passwd = "13 orginal colonies"; 
-// var msg = "What do the stripes on the American Flag repersent?";
-
-// do {
-//   msg === passwd 
-// }
-
-
-
-
-// function taskOne(response) {
-
-// if (response === "As American As it Gets") {
-//    alertMessage = "Well, You have an American Day";
-//     color = "blue";
-// }   //else {
-//     alertMessage = "That's Alright, America is land of the Free and home of the Brave";
-//     color = "red";
-// }
-
-// document.getElementById("result").style.backgroundColor = red;
-
-// function addPresidentsToDocument (President) {
-//    document.write("<selection> \
-//                            <p1>" + President + "</p1> \
-//                            <img presidentImg" + President + ".png\*> \
-//                            <h1>" + President + " Was the best!</h1> \
-//                            </selection>"); 
-// }
-
-// addPresidentsToDocument("Roosevelt");
-// addPresidentsToDocument("Lincon");
-// addPresidentsToDocument("Washington");
-
-// = url("https://cdn.britannica.com/76/4476-050-E643DD49/Betsy-Ross-legend-flag.jpg")
+} else if (response == "1775") {
+  alertMessage = "close! Thats the Army's Birthday though!";
+  backgroundColor = red;
+}
