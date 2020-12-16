13d4b8cf907c92133cff3f348a6081a15ca8121e
Anthony Joel Williams <williamsjanthony15@yahoo.com>
2020-12-15 23:28:45 -0600

    Adding for Adam to do Adam Things, Line 63 Commented Line throwing Noncommented Areas in JS console log.

--- app.js
+++ app.js
@@ -10,37 +10,54 @@ function add(a, b) {
     var result = ("17" + 76);
    alert(result);
 
+// for (var A = 0; A < 10; A+++) {
+//   document.write(A)
+// }
+for (var A = 0; A >= 100; A++) {
+  console.log
+}
+
+function ValidateUserPassword() {
+    var passwd = "13 Orginal Colonies";
+    var pass;
+
+    while(pass === passwd){
+
+    }
+// }
+
+// var passwd = "13 orginal colonies"; 
+// var msg = "What do the stripes on the American Flag repersent?";
+
+// do {
+//   msg === passwd 
+// }
+
+
+
 
 // function taskOne(response) {
 
-//if (response === "As American As it Gets") {
-  //  alertMessage = "Well, You have an American Day";
-    //color = "blue";
+// if (response === "As American As it Gets") {
+//    alertMessage = "Well, You have an American Day";
+//     color = "blue";
 // }   //else {
-    //alertMessage = "That's Alright, America is land of the Free and home of the Brave";
-    //color = "red";
-//}
+//     alertMessage = "That's Alright, America is land of the Free and home of the Brave";
+//     color = "red";
+// }
 
-//document.getElementById("result").style.backgroundColor = red;
+// document.getElementById("result").style.backgroundColor = red;
 
-//function addPresidentsToDocument (President) {
-  //  document.write("<selection> \
-    //                        <p1>" + President + "</p1> \
-      //                      <img presidentImg" + President + ".png\*> \
-        //                    <h1>" + President + " Was the best!</h1> \
-          //                  </selection>"); 
-//}
+// function addPresidentsToDocument (President) {
+//    document.write("<selection> \
+//                            <p1>" + President + "</p1> \
+//                            <img presidentImg" + President + ".png\*> \
+//                            <h1>" + President + " Was the best!</h1> \
+//                            </selection>"); 
+// }
 
-//addPresidentsToDocument("Roosevelt");
-//addPresidentsToDocument("Lincon");
-//addPresidentsToDocument("Washington");
+// addPresidentsToDocument("Roosevelt");
+// addPresidentsToDocument("Lincon");
+// addPresidentsToDocument("Washington");
 
 // = url("https://cdn.britannica.com/76/4476-050-E643DD49/Betsy-Ross-legend-flag.jpg")
-
-
-
-// Also done correctly but no call or declare for the colors. Your not saying what the colors are for nor what there supposed to do. Take the free code that JS gives you.... document.getElecmentById("content").style.backgroundColo = color;
-
-//important
-//document.getElecmentById("content").style.backgroundColo = ;
-//document.getElecmentById("content").style.backgroundColo = color;
