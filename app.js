57aeba75ba2bfd9b5ad69dbd3d41dc98a72c256a
Anthony Joel Williams <williamsjanthony15@yahoo.com>
2020-12-17 02:11:36 -0600

    Outline and fundementals worked on

--- app.js
+++ app.js
@@ -29,15 +29,15 @@ if (response === "1787") {
     console.log(i)
 }
 
-function getUserPrompt(message)
+function getUserPrompt(??)
 
-  var userInput = prompt(message);
+  var userInput = prompt(??);
 
-  return userInput;
+  return userInput; //??
 
-  
+
 // First President Add
-function addPresidentToDocument(PresidentName, Year) {
+function addPresidentToDocument(presidentName) {
 
   if(presidentName === "John F Kennedy") {
       description = "John F. Kennedy was the 35th President of the United States (1961-1963), the youngest man elected to the office. On November 22, 1963, when he was hardly past his first thousand days in office, JFK was assassinated in Dallas, Texas, becoming also the youngest President to die."
@@ -46,15 +46,64 @@ function addPresidentToDocument(PresidentName, Year) {
 
 // Pictures, And links to HTML for header / Description
 
-  Document.write("<section> \
-                    <h2> + presidentName + "</h2> \
-                    <img src=\"images/" + presidentName + ".png\"> \
-                    <p>" + presidentName + description + ".</p> \
-                    </section>");
-}
+//   Document.write("<section> \
+//                     <h2> + presidentName + "</h2> \
+//                     <img src=\"images/" + presidentName + ".png\"> \
+//                     <p>" + presidentName + description + ".</p> \
+//                     </section>");
+// }
 
 function sayMessage(message) {
     alert(message);
 }
 // We Welcome you, the President(s) of the Great United States
 
+function setColorBasedOnUserInput(userResponse) {
+
+  var color = blue;
+
+  if (userResponse === "Abraham Lincoln") {
+    color = "red";
+  } else if (userResponse === "Richard Nixon") {
+    color = "";
+  } else if (userResponse === "") {
+    color = "";
+  } else if (getUserPrompt === "") {
+    color = "";
+  }
+
+
+// requires HTML element with the label of "Content"
+
+document.getElementById("content").style.backgroundColor = color; 
+}
+
+function validateUserPassword() {
+  var storedPassword = "Honest Abe";
+  
+  do {
+    var passwd = getUserPrompt("enter a password");
+  }while(passwd !== storedPassword);
+
+}
+
+validateUserPassword();
+
+var promptQuestion = "Who was the President during the Civil War?";
+
+var response = getUserPrompt(promptQuestion);
+
+setColorBasedOnUserInput(response);
+
+addPresidentToDocument("Abraham Lincoln", "Honest Whether you like it or not");
+addPresidentToDocument("Richard Nixon" , "Dont get much better than that");
+
+var PresidentName = "George Washington";
+var description = "First Ever President.";
+addPresidentToDocument(preidentName, description);
+
+var numberOfPresidents = getUserPrompt("How many presidents do/did you like?");
+for (var i = 0; 1 < numberOfPresidents; i++) {
+  console.log(i);
+  addPresidentToDocument("George Bush", "Jr or Sr"?;
+}
