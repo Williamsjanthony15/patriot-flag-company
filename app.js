0af181e2bb0769b42c138bad6b85f82852a4d47d
Anthony Joel Williams <williamsjanthony15@yahoo.com>
2020-12-16 20:00:09 -0600

    Added a loop and trying for a function as well but getting stuck up on the password issue.

--- app.js
+++ app.js
@@ -1,18 +1,19 @@
-var myPrompt = "Patriot Flag Company";
-console.log(myPrompt);
-var alertMessage; 
 
-var promptQuestion = "What year was the American Constitution signed?";
-var response = prompt("what year was the American Constitution signed"); {
-  document.log() 
+var myPrompt = "Patriot Flag Company"; {
+console.log(myPrompt);
 }
 
+
+var promptQuestion = "What year was the American Constitution signed?";
+var response = prompt(promptQuestion);
 
 if (response === "1787") {
   alertMessage = "Correct! Welcome!";
-  backgroundColor = blue;
 
-} else if (response == "1775") {
-  alertMessage = "close! Thats the Army's Birthday though!";
-  backgroundColor = red;
+} else  {
+  alertMessage = "I guess you don't know.";
+} 
+ {
+  window.alert(alertMessage);
 }
+
